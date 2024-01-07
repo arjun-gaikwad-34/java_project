183d880751fa3af141850755f137da77b2a606f3
arjun-gaikwad-34 <arjun.gaikwad@genzeoon.com>
2024-01-07 23:58:38 +0530

    id generator added

--- src/Entity/Book.java
+++ src/Entity/Book.java
@@ -6,3 +6,18 @@ public class Book {
     public String title;
 
 }
+
+private void generateRandomId() {
+    // Generate a 5-digit alphanumeric code
+    int length = 5;
+    String alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    Random random = new Random();
+    StringBuilder idBuilder = new StringBuilder();
+
+    for (int i = 0; i < length; i++) {
+        char randomChar = alphanumeric.charAt(random.nextInt(alphanumeric.length()));
+        idBuilder.append(randomChar);
+    }
+
+    this.id = Long.valueOf(idBuilder.toString());
+}
