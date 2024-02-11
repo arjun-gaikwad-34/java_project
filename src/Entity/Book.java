90b19570a8e9d9c7ba54261f3f019904206eb223
arjun-gaikwad-34 <arjun.gaikwad@genzeoon.com>
2024-02-11 20:31:02 +0530

    back update

--- src/Entity/Book.java
+++ src/Entity/Book.java
@@ -1,11 +1,38 @@
 package Entity;
 
+import java.util.Random;
+
 public class Book {
 
-    public Long id;
-    public String title;
+    generateRandomId();
+
 
 }
+
+public Book(String title, String author, String description, String publisher, LocalDateTime publishedDate) {
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.publisher = publisher;
+    this.publishedDate = publishedDate;
+    generateRandomId();
+}
+
+private void generateRandomId() {
+    // Generate a 5-digit alphanumeric code
+    int length = 5;
+    String alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    Random random = new Random();
+    StringBuilder idBuilder = new StringBuilder();
+
+    for (int i = 0; i < length; i++) {
+        char randomChar = alphanumeric.charAt(random.nextInt(alphanumeric.length()));
+        idBuilder.append(randomChar);
+    }
+
+    this.id = Long.valueOf(idBuilder.toString());
+}
+
 
 private void generateRandomId() {
     // Generate a 5-digit alphanumeric code
